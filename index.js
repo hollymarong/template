05b39d8d36fa8f82cb4de63939977c0702efbb90
marong <marong@marongdeMacBook-Pro.local>
2017-05-20 15:08:07 +0800

    delete test.js

--- index.js
+++ index.js
@@ -1,71 +1,21 @@
-var $$ = bowlder;
-debugger;
-// function genFunc(expr, obj){
-//     console.log('expr', expr, 'obj', obj);
-//     var func = new Function('obj', 'with(obj)' + 'return '+expr);
-//     console.log(func);
-//     return func(obj);
-// }
-// var template = {
-//     replace: function(tpl, data){
-//         var ret = [];
-//         data.forEach(function(item){
-//             ret.push(replaceAction(item));
-//         });
-//         function replaceAction(obj){
-//             return tpl.replace((/\{\{(.*?)\}\}/g), function(match, name){
-//                 var result = genFunc(name, obj);
-//                 console.log('result',result);
-//                 return result;
-//             });
-//         }
-//         return ret.join("");
-//     }
-// };
-// var dd = $$.template.replace('{{name}}: {{age}}:{{loves.drink}}', [ {name:'bill',age:20,loves:{drink:'tea'}} ]);
-// console.log('dd', dd);
-
-// var cc = template.replace('{{name}}: {{age}}:{{loves.drink}}', [ {name:'bill',age:20,loves:{drink:'tea'}} ]);
-// console.log('cc', cc);
-var template = {
-    parse: function(tpl, data){
-        var source = "var __t,__p='';\nwith(obj||{}){__p+='";
-        var index = 0;
-        var escapes = {
-            "'":      "'",
-            '\\':     '\\',
-            '\r':     'r',
-            '\n':     'n',
-            '\t':     't',
-            '\u2028': 'u2028',
-            '\u2029': 'u2029'
-        };
-        var escapeMatch = /\\|'|\r|\n|\t|\u2028|\u2029/g,
-            escaper = function(match) { return '\\' + escapes[match]; };
-        tpl.replace(/<%=(.*?)%>|<%(.*?)%>|$/g, function(match, interpolate, evaluate, offset){
-            // source += tpl.slice(index, offset).replace(escapeMatch, escaper);
-            source += tpl.slice(index, offset).replace(escapeMatch, escaper);
-            if(interpolate){
-                source += "'+\n"+interpolate+"+'";
-            }else if(evaluate){
-                source += "';\n" + evaluate + "\n__p+='";
-            }
-            index = offset + match.length;
-            return match;
-
-        });
-        source += "';\n}return __p;";
-        console.log(source);
-        var fn = new Function('obj', source);
-        return fn(data);
+var data = {
+    name:'marong',
+    age: 27,
+    loves:{
+        drink:'tea'
     }
 };
-var arrowTemplate = $$.template.parse('<%=name%>: <%=age%>', {name:'bill',age:20});
-console.log('arrowTemplate', arrowTemplate);
 
-var arrowTemplate = template.parse('<%=name%>: <%=age%>', {name:'bill',age:20});
-console.log('my arrowTemplate', arrowTemplate);
+var tpl = 'hello {{name}}, age is {{age}}, loves drink is {{loves.drink}}';
+var result = template.replace(tpl, data);
+console.log('curly  result', result);
+
+var tpl = 'hello <%=name%>, age is <%=age%>, loves drink is <%=loves.drink%>, loves fruit is <%=loves.fruit%>';
+var result = template.parse(tpl, data);
+console.log('arrow result', result);
+
 var tpl = document.getElementById('templateScript').innerHTML;
+
 var data = {desc:'hello template',
             name:'marong',
             age:28,
@@ -85,9 +35,6 @@ var data = {desc:'hello template',
                 }
             ]
            };
-debugger;
-// var result = $$.template.parse(tpl, data);
-// console.log('$$result', result);
 var result = template.parse(tpl, data);;
 console.log('result', result);
 document.body.innerHTML = result;
--- template.js
+++ template.js
@@ -19,24 +19,34 @@ var template = {
     parse: function(tpl, data){
         var index = 0;
         var arrowReg = /<%=(.*?)%>|<%(.*?)%>|$/g;
+        // var escapeMap = {
+        //     "\r":"r",
+        //     "\n":"n",
+        //     "\t":"t",
+        //     "'":"'"
+        // };
         var escapeMap = {
-            "\r":"r",
-            "\n":"n",
-            "\t":"t",
-            "'":"'"
-        };
-        var escapeReg = /\r|\n|\t|'/g;
+            "'":      "'",
+            '\\':     '\\',
+            '\r':     'r',      // 回车
+            '\n':     'n',      // 换行符
+            '\t':     't',      // Tab
+            '\u2028': 'u2028',  // 行分割符
+            '\u2029': 'u2029'   // 段落分割符
+        };;
+        // var escapeReg = /\r|\n|\t|'/g;
+        var escapeReg = /\\|'|\r|\n|\t|\u2028|\u2029/g;
         var escapeFunc = function(match){
             return "\\" + escapeMap[match];
         };
         var source = "var __p='',__t='';with(data||{}){__p+='";
         tpl.replace(arrowReg, function(match, expr, evaluate, offset){
-            source += tpl.slice(index, offset).replace(escape, escapeFunc);
+            source += tpl.slice(index, offset).replace(escapeReg, escapeFunc);
             if(expr){
                 console.log('expr',expr);
                 source += "';__t=" + expr + ";(!!__t||__t==0)&&(__p+=__t);__p+='";
             }else if(evaluate){
-                source += "';\n" + evaluate + ";__p+='";
+                source += "';\n" + evaluate + "__p+='";
             }
             index = offset + match.length;
             return match;
@@ -48,18 +58,3 @@ var template = {
     }
 };
 
-var data = {
-    name:'marong',
-    age: 27,
-    loves:{
-        drink:'tea'
-    }
-};
-
-var tpl = 'hello {{name}}, age is {{age}}, loves drink is {{loves.drink}}';
-var result = template.replace(tpl, data);
-console.log('curly  result', result);
-
-var tpl = 'hello <%=name%>, age is <%=age%>, loves drink is <%=loves.drink%>, loves fruit is <%=loves.fruit%>';
-var result = template.parse(tpl, data);
-console.log('arrow result', result);
